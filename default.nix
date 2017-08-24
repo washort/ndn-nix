4020cc150b6423850db66ab94baa1c352e7751e5
Allen Short <ashort@mozilla.com>
2017-08-24 02:24:24 +0000

    typo

--- default.nix
+++ default.nix
@@ -4,5 +4,5 @@ in
 {
   ndn-cxx = ndnCxx;
   nfd = pkgs.callPackage ./nfd.nix { ndn-cxx = ndnCxx; };
-  ndn-tools = pkgs.callPackage ./ndn-tools.nix {};
+  ndn-tools = pkgs.callPackage ./ndn-tools.nix {ndn-cxx = ndnCxx; };
 }
